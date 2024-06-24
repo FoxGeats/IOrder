6fc7d04b35c11f2c194048d847fe8af825711bbf
Shu Chen <GGGGeats@gmail.com>
2024-06-24 15:12:33 +0800

    Day01-foxapi-swagger-end

--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -39,8 +39,9 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        // 对前端传入的密码进行md5加密，然后再进行比对
+        String p= DigestUtils.md5DigestAsHex(password.getBytes());
+        if (!p.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
